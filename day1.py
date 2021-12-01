d3f29f02f83655e446f01472c51034b11dddc682
Puuuul-cmd <psts@tmo.at>
2021-12-01 20:55:08 +0100

    Day 1

--- day1.py
+++ day1.py
@@ -1,1 +1,36 @@
+def method1():
+    file1 = open("input files/day1.txt")
+    Lines = file1.readlines()
+    ints = []
+    for line in Lines:
+        line.strip()
+        ints += [int(line)]
+    count = 0
 
+    for x in range(len(ints)):
+        if ints[x] > ints[x-1]:
+            count += 1
+
+    print(count)
+
+
+def method2():
+    file2 = open("input files/day1txt")
+    Lines = file2.readlines()
+    ints = []
+    for line in Lines:
+        line.strip()
+        ints += [int(line)]
+    count = 0
+
+    for x in range(len(ints)-2):
+        first = ints[x-1]+ints[x]+ints[x+1]
+        second = ints[x]+ints[x+1]+ints[x+2]
+        if first < second:
+            count += 1
+
+    print(count)
+
+
+method1()
+method2()
