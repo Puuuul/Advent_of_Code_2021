718471c930b48f8305661a5c2c28b4f85f0832b6
Puuuul-cmd <psts@tmo.at>
2021-12-06 09:51:51 +0100

    Day 6

---
+++ day6.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+
+def method1():
+    file1 = open("input files/day6.txt")
+    s = file1.readline()
+    arr = s.split(",")
+    fishcount = deque([0 for i in range(9)])
+    for fish in arr:
+        fishcount[int(fish)] += 1
+    for x in range(256):
+        fishcount.rotate(-1)
+        fishcount[6] += fishcount[8]
+        print(fishcount)
+    print(sum(fishcount))
+
+
+method1()
